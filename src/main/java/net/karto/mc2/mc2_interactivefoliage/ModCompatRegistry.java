90b0a8c2c3fff0fb33715c477132e46a37872b19
Kart0 <40776836+Kart0@users.noreply.github.com>
2026-09-13 00:26:13 -0500

    Foliage compat: sway Biomes O' Plenty's barley

--- src/main/java/net/karto/mc2/mc2_interactivefoliage/ModCompatRegistry.java
+++ src/main/java/net/karto/mc2/mc2_interactivefoliage/ModCompatRegistry.java
@@ -66,6 +66,7 @@ public class ModCompatRegistry {
 				"biomesoplenty:dead_grass",
 				"biomesoplenty:tundra_shrub",
 				"biomesoplenty:bush",
+				"biomesoplenty:barley",
 				"biomesoplenty:enderphyte",
 				"biomesoplenty:tiny_cactus",
 				"biomesoplenty:yellow_maple_sapling",
